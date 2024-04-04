face1a75598ac6f4562bc367e4e9c024cfa19340
push edp <aspushedp@gmail.com>
2024-04-05 00:03:44 +1000

    update

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
-var Version string
-var Revision string
+var (
+    Version  = "0.0.0"
+    Revision = ""
+)
 
 func main() {
     fmt.Printf("Version: %s, Revision: %s\n", Version, Revision)
